c5dcb00285bbc4f58f172dc19840502646df5dcb
suryavanshiuma <suryavanshiu67@gmail.com>
2026-01-28 00:04:08 +0530

    27-01-2026

---
+++ 27-01-2026_List_Slicing.py
@@ -0,0 +1,13 @@
+l=[1, 2, 1.1, 2.2, "tops", 101, True, 10, 20, "python", False, 90.90, 1, 2, 1000, 2000, 3000]
+
+print(l[10:11:10])
+print(l[ :2:3])
+print(l[3: :12])
+print(l[ : :15])
+print(l[1:5:2])
+
+print(l[-13:-4:8])
+print(l[ :5:5])
+print(l[-12: :11])
+print(l[ : :-7])
+print(l[-10:-2:-2])
