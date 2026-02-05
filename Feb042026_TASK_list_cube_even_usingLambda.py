3fdb012f1ae60ef8d9b6b66071768034c0b5d54d
suryavanshiuma <suryavanshiu67@gmail.com>
2026-02-05 22:54:11 +0530

    05-02-2026

--- Feb042026_TASK_list_cube_even_usingLambda.py
+++ Feb042026_TASK_list_cube_even_usingLambda.py
@@ -1,17 +1,15 @@
-list = [1,2,3,4,5,6,7,8,9]
+
+list1 = [1,2,3,4,5,6,7,8,9]
 list2 = []
 list3 = []
 
 def cube(x):
     return x*x*x
 
-for i in list:
+for i in list1:
     list2.append(cube(i))
-
+print(list1)
 print(list2)
-print(list)
+list3 = list(filter(lambda j: j % 2 == 0, list2))
 
-for j in list2:
-##    list3 = list(filter(lambda j: j % 2 == 0, list2))
-    list3.append(filter(lambda j: j % 2 == 0, list2))
 print(list3)
